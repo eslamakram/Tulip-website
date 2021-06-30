0a6e9445fc974d8faeb9c3f6cd0086164a0711c6
ESLAMTWEIKAT <>
2021-06-30 15:00:59 +0000

    added script to indext

---
+++ App.js
@@ -0,0 +1,11 @@
+var getOrder = prompt("Do you want to buy a bouquet of tulips?")
+console.log(getOrder)
+if  (getOrder == "yes" |  getOrder == "true" ){
+  var customerName = prompt ("Kindly Could you give us your name and which type you want to buy?")
+  console.log(customerName)
+  document.write (customerName)
+}
+else {
+  alert(" I hope see you again here!")
+}
+
